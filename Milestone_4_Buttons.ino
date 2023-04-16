6b5503db7af534d571db706bf2668fa6d01b2e86
mjohn98 <112355340+mjohn98@users.noreply.github.com>
2023-04-15 21:39:45 -0500

    Add files via upload

--- Milestone_4_Buttons.ino
+++ Milestone_4_Buttons.ino
@@ -1,18 +1,71 @@
-// PSEUDOCODE for next steps:
-// Determine how far away a train the user is tracking is, and update timeAway to play the correct buzzer sound
-int timeAway = 5;
+// References: https://www.youtube.com/watch?v=AgQW81zzR18&t=1216s 
+// https://www.youtube.com/watch?v=aMato4olzi8
+// https://www.circuito.io/app?components=512,11021 
+
+int LEDPin = 12;
+int LEDPinTwo = 8;
+
+int buttonPin = 7;
+int buttonPinTwo = 10;
+int buttonNew; // reads in current value
+int buttonNewTwo; 
+
+int buttonOld=1; // is 1 before you press button
+
+int buttonOldTwo=1; // is 1 before you press butto
+int dt=100;
+bool lineSelected = false;
+bool stopSelected = false;
+
+int userLine;
+int userStop; 
 
 void setup() {
-  if (timeAway == 15){
-    tone(buzzerPin, 1500, 100);
-  } 
-  if (timeAway == 10){
-    tone(buzzerPin, 1500, 100);
-  } 
-  if (timeAway == 5){
-    tone(buzzerPin, 2500, 100);
-  }
+  Serial.begin(9600);
+  // light output
+  pinMode(LEDPin, OUTPUT);
+  pinMode(LEDPinTwo, OUTPUT);
+  // digitalWrite(LEDPinTwo, HIGH);
+  // digitalWrite(LEDPin, HIGH);
+  // button input
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonPinTwo, INPUT);
+
+
 }
 
+int index = 1;
+int stopIndex = 1;
 void loop() {
-}
+    buttonNew = digitalRead(buttonPin);
+    buttonNewTwo = digitalRead(buttonPinTwo);
+    if(buttonOld==0 && buttonNew==1){
+        // swap the line selection
+        if(index == 1){
+          index = 2;
+        }
+        else{
+          index = 1;
+        }
+        Serial.print("Current Index: ");
+        Serial.println(index);
+        userLine = index;
+    }
+    buttonOld=buttonNew;
+
+    // confirm input selected
+    if(buttonOldTwo==0 && buttonNewTwo==1){
+        Serial.print("Selected Index: ");
+        Serial.print(index);
+        digitalWrite(LEDPin, HIGH);
+        // if line is already selected, this means the input to confirm is the stop
+        if(lineSelected){
+          stopSelected = true;
+          digitalWrite(LEDPinTwo, HIGH);
+        }
+        lineSelected = true;
+    }
+    buttonOldTwo=buttonNewTwo;
+    // deal with the stop index
+    delay(dt);
+}  
